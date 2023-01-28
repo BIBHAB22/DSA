33955ccd0338ced443245be4048d5e4719e7850a
BIBHAB22 <113331341+BIBHAB22@users.noreply.github.com>
2023-01-28 08:08:05 +0530

    Update Heapsort.cpp

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -1,49 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-void adjustHeap(int arr[],int i,int n){
-	int j=2*i;
-	int key=arr[i];
-	while(j<=n){
-		if(j<n && arr[j]<arr[j+1])
-			j=j+1;
-		if(key>arr[j])
-			break;
-		arr[j/2]=arr[j];
-			j=j*2;
-	}
-	arr[j/2]=key;
+void swap(int *a,int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
 }
-void makeHeap(int arr[],int n){
-	for(int i=n/2;i>=1;i--)
-		adjustHeap(arr,i,n);
-}
-void heapsort(int arr[],int n){
-	makeHeap(arr,n);
-	for(int i=n;i>=2;i--){
-		int temp=arr[1];
-		arr[1]=arr[i];
-		arr[i]=temp;
-		adjustHeap(arr,1,i-1);
-	}
-}
-void show(int arr[],int s) 
-  {
+void show(int arr[],int s){
   	int i;
-  	for(i=0;i<s;i++)
-  	  {
+  	for(i=0;i<s;i++){
   	      printf("%4d",arr[i]);	
 	  }
 	  printf("\n");
    } 
+void heapify(int arr[], int n, int i){
+	int largest = i;
+	int left = 2*i + 1;
+	int right = 2*i + 2;
+
+	if(left < n && arr[left] > arr[largest]){
+		largest = left;
+	}
+	if(right < n && arr[right] > arr[largest]){
+		largest = right;
+	}
+	if(i != largest){
+		swap(&arr[i],&arr[largest]);
+		heapify(arr,n,largest);
+	}
+}	
+void heapSort(int arr[], int n){
+	for(int i = n/2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+	for(int i = n-1; i >= 0; i--){
+		swap(&arr[i],&arr[0]);
+		heapify(arr, i, 0);
+	}
+}
 int main(){
 		int arr[100];
 		int s,i;
 		printf("Enter the number:");
 		scanf("%d",&s);
 		for(i=0;i<s;i++){
-			printf("Enter number %d:-  ",(i+1));
+			printf("Enter number %d:- ",(i+1));
 		    scanf("%d",&arr[i]);	
 		}
-		heapsort(arr,s);		
+		heapSort(arr,s);		
 		show(arr,s);
 }
